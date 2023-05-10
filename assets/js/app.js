ba197ac5d880c55b48a7f31f328b973523fe039b
Youssef Ghareb <youssef.ghareb99@gmail.com>
2023-05-10 16:05:26 +0300

    Code

---
+++ assets/js/app.js
@@ -0,0 +1,72 @@
+var data = [{
+    iconClasses: ["fa-regular", "fa-star"],
+    title: "الأفضل مشورة",
+    text: "بالإضافة إلى استقلاليتنا في دراية، فقد طورنا العديد من الآليات الحصرية التي تساعد المستثمر على تكوين محافظ متنوعة وعلى انتقاء المنتجات الأفضل."
+}, {
+    iconClasses: ["fa-regular", "fa-face-smile"],
+    title: "الأكثر راحة",
+    text: "لن تحتاج إلى فتح حسابات متعددة؛ أو حفظ أسماء دخول مختلفة لأن دراية تتيح لك الاستثمار في كافة المنتجات الاستثمارية من مكان واحد، بل أصبح بإمكانك متابعة كل استثماراتك وإدارتها عبر موقع واحد وحساب واحد."
+}, {
+    iconClasses: ["fa-solid", "fa-folder-open"],
+    title: "الأكثر تنويعاً",
+    text: "تمكنك دراية من الاستثمار في أكثر من 60 منتج استثماري من 10 مدراء مختلفين. هذه المنتجات تستثمر في كافة الفئات الاستثمارية (كالأسهم، والصكوك، والمرابحة...إلخ) والأسواق كما يمكنك التداول في ما يزيد عن 1,200 من صناديق المؤشرات المتداولة ETF"
+}, {
+    iconClasses: ["fa-solid", "fa-dollar-sign"],
+    title: "و بلا تكلفة اضافية",
+    text: "كافة المنتجات الاستثمارية متاحة بنفس التكلفة المتاحة عند الاستثمار مع الجهة الاستثمارية مباشرة (أي بدون أي رسوم إضافية)."
+},
+{
+    iconClasses: ["fa-solid", "fa-arrow-trend-up"],
+    title: "الأكثر شفافية",
+    text: "أصبح من السهل عليك المقارنة بين المنتجات التي تقدمها دراية من خلال حساب موحد بشكل شفاف لانتقاء الأفضل من بينها."
+},
+{
+    iconClasses: ["fa-regular", "fa-thumbs-up"],
+    title: "الأكثر استقلالية",
+    text: "دراية تركز على جمع كافة المنتجات الاستثمارية تحت سقف واحد بدلأ من التركيز على بيع منتجات بعينها، الأمر الذي يعطيها قدر أكبر من الاستقلالية والحيادية في تعاملها ومشورتها."
+},
+]
+
+window.onload = function () {
+    var parent = document.querySelector(".cards");
+    var row = document.createElement("div");
+    row.classList.add("row");
+    parent.append(row);
+
+    for (var i = 0; i < data.length; i++) {
+        var col = document.createElement("div");
+        col.classList.add("col-md-4");
+        var card = document.createElement("div");
+        card.classList.add("card");
+        var iconContainer = document.createElement("div");
+        iconContainer.classList.add("icon-container");
+        var icon = document.createElement("i");
+        icon.classList.add(...data[i].iconClasses);
+        iconContainer.append(icon);
+        card.append(iconContainer);
+        var title = document.createElement("h4");
+        title.innerText = data[i].title;
+        card.append(title);
+        var text = document.createElement("p");
+        text.innerText = data[i].text;
+        card.append(text);
+        col.append(card);
+        row.append(col);
+    }
+
+    document.getElementById("purple-theme").onclick = function () {
+        var allElements = document.querySelectorAll(".bg-orange");
+        for (var i = 0; i < allElements.length; i++) {
+            allElements[i].classList.remove("bg-orange");
+            allElements[i].classList.add("bg-purple");
+        }
+    }
+
+    document.getElementById("orange-theme").onclick = function () {
+        var allElements = document.querySelectorAll(".bg-purple");
+        for (var i = 0; i < allElements.length; i++) {
+            allElements[i].classList.remove("bg-purple");
+            allElements[i].classList.add("bg-orange");
+        }
+    }
+}
